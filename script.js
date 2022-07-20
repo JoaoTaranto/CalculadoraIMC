5ad83aee7a8ecd7d86d2651d7047d4810a187132
JoaoTaranto <tarantoj04@gmail.com>
2022-07-20 14:48:48 -0300

    Aperfeiçoamento JS

--- script.js
+++ script.js
@@ -8,9 +8,19 @@ function calcular() {
     var alturas2 = alturaformatada * alturaformatada;
     var calculoIMC = parseInt(peso / alturas2)
 
-
-    if (calculoIMC == NaN) {
+    if (isNaN(peso)) {
         campoResultado.innerHTML = "Digite valores válidos";
+    } else if (calculoIMC < 18.6) {
+        campoResultado.innerHTML = "Olá " + nome + ", seu peso IMC é de " + calculoIMC + ", você está abaixo do peso!";
+    } else if (calculoIMC < 25) {
+        campoResultado.innerHTML = "Olá " + nome + ", seu peso IMC é de " + calculoIMC + ", você está com peso adequado.";
+    } else if (calculoIMC < 30) {
+        campoResultado.innerHTML = "Olá " + nome + ", seu peso IMC é de " + calculoIMC + ", você está com sobrepeso, cuidado."
+    } else if (calculoIMC < 35) {
+        campoResultado.innerHTML = "Olá " + nome + ", seu peso IMC é de " + calculoIMC + ", você está com obesidade 1° Grau!"
+    } else if (calculoIMC < 40) {
+        campoResultado.innerHTML = "Olá " + nome + ", seu peso IMC é de " + calculoIMC + ", você está com obesidade 2° Grau!"
+    } else if (calculoIMC >= 40) {
+        campoResultado.innerHTML = "Olá " + nome + ", seu peso IMC é de " + calculoIMC + ", você está com obesidade 3° Grau/Mórbida!!"
     }
-    campoResultado.innerHTML = "Olá " + nome + ", seu peso IMC é de " + calculoIMC;
 }
